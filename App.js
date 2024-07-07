fa153a090f24b8adb551b8a71aac2f977d895330
PimKalasinmongkol <pimty.pim@gmail.com>
2024-07-07 14:17:54 +0700

    ver0

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
   return (
     <SafeAreaProvider>
